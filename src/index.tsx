9a996ad7d51f645318b38373f3bc868822b95027
InfinitePower563 <nathan-lee1111@outlook.com>
2023-11-13 21:21:17 -0600

    Initial Commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import {About, App, NotFound, Projects, WIP} from './App';
 import reportWebVitals from './reportWebVitals';
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
 
+const router = createBrowserRouter(
+    [
+        {path: '/', element: <App />},
+        {path: '/about', element: <About />},
+        {path: '/projects', element: <Projects />},
+        {path: '/contact', element: <WIP />},
+        {path: '/blog', element: <WIP />},
+        {path: '/*', element: <NotFound />},
+    ]
+);
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
