f980155af313cd4c678629623b42d2f4994bd8a0
Nathan Lee <me@nwlee.tech>
2025-05-06 10:18:37 -0500

    update packages + home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,7 @@ const texts = [
     new ContentText("<br />Hello! Welcome to my website!", false, delays.very_short),
     new ContentText("My name's Nathan, I'm a software engineer and full time high school student.", false, delays.very_short),
     new ContentText("I have many activities that I enjoy doing. I'll list some here.<br/>", false, delays.very_short),
-    new ContentText(" → Programming <br /> → Flying flight simulators <br /> → Playing video games", false, delays.very_short),
+    new ContentText(" → Programming <br /> → Flying flight simulators <br /> → Playing video games <br /> → Day trading", false, delays.very_short),
     new ContentText("I also have a few projects I'm working on, and some that are already completed.", false, delays.very_short),
     new ContentText("#navbar#", true, delays.very_short),
 ];
